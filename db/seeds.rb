a5d7a100376de7ff90c5193c10eeadf994aecb90
Ankush <ankushjadevv4tech@gmail.com>
2025-02-07 18:00:56 +0530

    Update seed file to add test user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.find_or_initialize_by(email: 'test@example.com').tap do |user|
+  user.password = 'password'
+  user.password_confirmation = 'password'
+  user.save!
+end
