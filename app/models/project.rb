22147a66bab9649376a20809450b8126848b8fa3
Ankush <ankushjadevv4tech@gmail.com>
2025-02-07 19:53:52 +0530

    Added Status to project table

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,6 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
+  enum :status, { draft: 0, appoved: 1, delivered: 2 }
   validates :title, :details, presence: true
 end
